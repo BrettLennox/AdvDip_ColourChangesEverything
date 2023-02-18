3984ab109923ab4ce122df71b96ab3132f4f2137
BrettLennox <brettheaton@hotmail.com>
2023-02-18 18:31:42 +1100

    tilemaps and assets

--- Assets/Scripts/Platforms/PlatformCycleDisplay.cs
+++ Assets/Scripts/Platforms/PlatformCycleDisplay.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
-public class PlatformCycleDisplay : CycleDisplay
+public class PlatformCycleDisplay : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    #region Variables
+    [SerializeField] private Tilemap _tilemap;
+    [SerializeField] Colours _objectColour;
+    [SerializeField] private Collider2D _collider;
+    #endregion
 
     // Update is called once per frame
     void Update()
     {
-        base.CycleSpriteRenderer();
+        CycleSpriteRenderer();
+        _collider.enabled = _objectColour == ChooseColour.instance.ChosenColour ? true : false;
+    }
+
+    private void CycleSpriteRenderer()
+    {
+        _tilemap.color = _objectColour == ChooseColour.instance.ChosenColour ? Colour.instance.ReturnActiveColour(_objectColour) : Colour.instance.ReturnInactiveColour(_objectColour);
     }
 }
