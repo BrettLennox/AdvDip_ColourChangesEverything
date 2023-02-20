5db97320804537ea43ed4af241e4195cd26293b7
BrettLennox <brettheaton@hotmail.com>
2023-02-20 17:31:13 +1100

    UI Changes

---
+++ Assets/Scripts/Player/PlayerCollisionCheck.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisionCheck : MonoBehaviour
+{
+    [SerializeField] private string _enemyTag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
